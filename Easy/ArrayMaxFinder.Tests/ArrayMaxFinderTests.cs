e4decd9356bebc0c71784277f46ed482e3f5323f
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2026-06-19 07:08:45 +0000

    Implement FindMax method and add FindTopTwo test cases in ArrayMaxFinderTests

--- Easy/ArrayMaxFinder.Tests/ArrayMaxFinderTests.cs
+++ Easy/ArrayMaxFinder.Tests/ArrayMaxFinderTests.cs
@@ -5,10 +5,22 @@ public class ArrayMaxFinderTests
 {
     
     [Fact]
-    public void FindMax_GivenFiveNums_ShoulddReturnHighest()
+    public void FindMax_GivenFiveNums_ShouldReturnHighest()
     {
         int[] nums = [6, 9, 2, 0, 1];
         int expected = 9;
+
+        var arrayMaxFinder = new ArrayMaxFinder();
+        var max = arrayMaxFinder.FindMax(nums);
+
+        Assert.Equal(expected, max);
+    }
+
+    [Fact]
+    public void FindMax_GivenFiveNumsNeg_ShouldReturnHighest()
+    {
+        int[] nums = [-6, -9, -2, 0, 1];
+        int expected = 1;
 
         var arrayMaxFinder = new ArrayMaxFinder();
         var max = arrayMaxFinder.FindMax(nums);
@@ -38,5 +50,19 @@ public class ArrayMaxFinderTests
         var max = arrayMaxFinder.FindMax(nums);
 
         Assert.Equal(expected, max);
+    }
+
+    [Fact]
+    public void FindTopTwo_FiveGiven_ShoulddReturnTheTopTwo()
+    {
+        int[] nums = [6, 9, 2, 0, 1];
+        int expectedMax = 9;
+        int expectedSecond = 6;
+
+        var arrayMaxFinder = new ArrayMaxFinder();
+        (var max, var second) = arrayMaxFinder.FinddTopTwo(nums);
+
+        Assert.Equal(expectedMax, max);
+        Assert.Equal(expectedSecond, second);
     }
 }
--- Easy/ArrayMaxFinder/ArrayMaxFinder.cs
+++ Easy/ArrayMaxFinder/ArrayMaxFinder.cs
@@ -6,7 +6,18 @@ public class ArrayMaxFinder
 {
     public int FindMax(int[] nums)
     {
-        throw new NotImplementedException();
+        int currentMax = nums[0];
+
+        if(nums.Length == 1)
+            return currentMax;
+
+        for(int i = 1; i < nums.Length; i++)
+        {
+            if(nums[i] > currentMax)
+                currentMax = nums[i];
+        }
+
+        return currentMax;
     }
 
     public (int firstMax, int secondMax) FinddTopTwo(int[] nums)
