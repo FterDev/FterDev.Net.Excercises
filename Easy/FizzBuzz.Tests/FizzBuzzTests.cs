fb0b6421d1def931004898350c6d44f7484c169f
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2026-06-19 15:18:43 +0000

    Refactor FizzBuzz implementation and add comprehensive unit tests for FizzBuzz and FizzBuzzGeneral methods

---
+++ Easy/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,59 @@
+﻿namespace FterDev.Easy.FizzBuzz.Tests;
+
+public class FizzBuzzTests
+{
+    [Fact]
+    public void FizzBuzz_Given15_ReturnsCorrectList()
+    {
+        var expectedList = new List<string>()
+        {
+          "1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"  
+        };
+
+        var res = Program.FizzBuzz(15);
+
+        Assert.Equal(expectedList, res);
+    }
+
+    [Fact]
+    public void FizzBuzzGeneral_Given15_ReturnsCorrectList()
+    {
+        var expectedList = new List<string>()
+        {
+          "1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"  
+        };
+        Dictionary<int, string> rules = new Dictionary<int, string>
+        {
+            {3, "Fizz"},
+            {5, "Buzz"}
+        };
+
+        var res = Program.FizzBuzzGeneral(15, rules);
+
+        Assert.Equal(expectedList, res);
+    }
+
+    [Fact]
+    public void FizzBuzzGeneral_ZeroRule_ThrowsArgumentException()
+    {
+    
+        Dictionary<int, string> rules = new Dictionary<int, string>
+        {
+            {0, "Fizz"},
+        };
+
+        Assert.Throws<ArgumentException>(() => Program.FizzBuzzGeneral(15, rules));
+    }
+
+    [Fact]
+    public void FizzBuzzGeneral_EmptyRule_ThrowsArgumentException()
+    {
+    
+        Dictionary<int, string> rules = new Dictionary<int, string>
+        {
+            {1, ""},
+        };
+
+        Assert.Throws<ArgumentException>(() => Program.FizzBuzzGeneral(15, rules));
+    }
+}
--- Easy/FizzBuzz/Program.cs
+++ Easy/FizzBuzz/Program.cs
@@ -5,17 +5,89 @@ public static class Program
     
     public static Task Main(string[] args)
     {
-        int num = 0;
 
-        Console.Write("Add an int for FizzBuzz:");
-        num = Console.Read();
+
+        int num = 100;
+        Dictionary<int, string> rules = new Dictionary<int, string>
+        {
+            {3, "Fizz"},
+            {5, "Buzz"}
+        };
+
+        var list = FizzBuzzGeneral(num, rules);
+        
+        PrintFizzBuzz(list);
 
         return Task.CompletedTask;
     }
 
-    public static void FizzBuzz(int n)
+    public static List<string> FizzBuzz(int n)
     {
-        
+
+        var list = new List<string>();
+
+        for(int i = 1; i <= n; i++)
+        {
+            var word = "";
+
+            if(i % 3 == 0)
+                word += "Fizz";
+            if(i % 5 == 0)
+                word += "Buzz";
+            
+            if(string.IsNullOrEmpty(word))
+            {
+                list.Add(i.ToString());
+            }
+            else
+            {
+                 list.Add(word);
+            }   
+        }
+
+        return list;
+    }
+
+    public static List<string> FizzBuzzGeneral(int n, Dictionary<int, string> rules)
+    {
+        RuleCheck(rules);
+        var list = new List<string>();
+
+        for(int i = 1; i <= n; i++)
+        {
+            var word = "";
+            bool ruleTriggered = false;
+
+            foreach((int r, string w) in rules)
+            {
+                if(i % r == 0)
+                {
+                    word += w;
+                    ruleTriggered = true;
+                }
+            }
+
+            list.Add(ruleTriggered ? word : i.ToString());
+        }
+
+        return list;
+    }
+
+    private static void PrintFizzBuzz(List<string> list)
+    {
+        foreach(string e in list)
+            Console.WriteLine(e);
+    }
+
+    private static void RuleCheck(Dictionary<int,string> rules)
+    {
+        foreach((int i, string w) in rules)
+        {
+            if(i == 0)
+                throw new ArgumentException("You can't use 0 as a rule");
+            if(string.IsNullOrEmpty(w))
+                throw new ArgumentException("Rule text can't be empty");
+        }
     }
 }
 
