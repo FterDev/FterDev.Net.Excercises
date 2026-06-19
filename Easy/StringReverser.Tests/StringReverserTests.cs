151cee2931989b7558d7cde9d15106226c33a2d7
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2026-06-19 09:44:20 +0000

    Implement ReverseString method with input validation for empty and single character strings

--- Easy/StringReverser.Tests/StringReverserTests.cs
+++ Easy/StringReverser.Tests/StringReverserTests.cs
@@ -1,10 +1,23 @@
 ﻿namespace FterDev.Easy.StringReverser.Tests;
 
-public class UnitTest1
+public class StringReverserTests
 {
-    [Fact]
-    public void Test1()
-    {
 
+    private readonly StringReverser _stringReverser;
+    public StringReverserTests()
+    {
+        _stringReverser = new StringReverser();
+    }
+
+    [Fact]
+    public void ReverseString_GivenEmptyString_ThrowArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => _stringReverser.ReverseString(""));
+    }
+
+    [Fact]
+    public void ReverseString_GivenoneCharString_ThrowArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => _stringReverser.ReverseString("a"));
     }
 }
--- Easy/StringReverser/StringReverser.cs
+++ Easy/StringReverser/StringReverser.cs
@@ -3,4 +3,15 @@
 public class StringReverser
 {
 
+    public string ReverseString(string input)
+    {
+        if(string.IsNullOrEmpty(input))
+            throw new ArgumentNullException("Empty strings are not allowed");
+
+        if(input.Length == 1)
+            throw new ArgumentException("One char strings not allowed");
+        
+        throw new NotImplementedException();
+    }
+
 }
